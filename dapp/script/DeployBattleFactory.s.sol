a31e70051412befbdeb20fa1000143cf830bd165
amadzai <20966134+amadzai@users.noreply.github.com>
2025-04-05 23:22:03 +0800

    feat: Update battle deployment script

--- dapp/script/DeployBattleFactory.s.sol
+++ dapp/script/DeployBattleFactory.s.sol
@@ -3,17 +3,28 @@ pragma solidity ^0.8.28;
 
 import "forge-std/Script.sol";
 import "../src/battle/BattleFactory.sol";
+import "../test/mocks/MockERC20.sol";
 
 contract DeployBattleFactory is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        
+        address deployer = vm.addr(deployerPrivateKey);
+
+        MockERC20 mockTokenA = new MockERC20("MockTokenA", "MOCKA", 18);
+        MockERC20 mockTokenB = new MockERC20("MockTokenB", "MOCKB", 18);
+
+        // Mint some tokens to the deployer for testing
+        mockTokenA.mint(deployer, 1000000 * 10 ** 18);
+        mockTokenB.mint(deployer, 1000000 * 10 ** 18);
+
         vm.startBroadcast(deployerPrivateKey);
-        
+
         BattleFactory battleFactory = new BattleFactory();
-        
+
         vm.stopBroadcast();
 
         console.log("BattleFactory deployed at:", address(battleFactory));
+        console.log("MockTokenA deployed at:", address(mockTokenA));
+        console.log("MockTokenB deployed at:", address(mockTokenB));
     }
 }
